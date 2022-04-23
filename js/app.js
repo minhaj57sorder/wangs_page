24ef1e15a8823ed1815c007c07029f7ce0bd625f
minhaj57sorder <74466041+minhaj57sorder@users.noreply.github.com>
2022-04-23 13:03:38 +0600

    delivery 3rd

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
+// faq expand collapse start
 const faq_heading = document.querySelectorAll(".faq-heading");
-
-
 faq_heading.forEach(e => {
-
     e.onclick = () => {
         e.parentNode.classList.toggle("collapsed")
         var content = e.nextElementSibling;
@@ -15,6 +13,8 @@ faq_heading.forEach(e => {
         }
     }
 })
+// faq expand collapse end
+// menu expand collapse start
 
 const menu_expand = document.getElementById("menu-expand");
 const header_links = document.getElementById("header-links");
@@ -26,35 +26,56 @@ menu_expand.onclick = () => {
 collaps_menu.onclick = () => {
     header_links.classList.add("header-collapsed-mobile")
 }
+// menu expand collapse end
 
-gsap.to("#shine", { x: 800, repeat: -1, repeatDelay: 3 })
+// parallax start
+// window.addEventListener("scroll", function () {
+//     let speedlimiter = 150;
+//     if (window.screen.width <= 678) speedlimiter = 50;
+//     let offset = this.window.pageYOffset;
+//     gsap.to("#paralax", { backgroundPositionY: -1 * offset / speedlimiter })
+// })
 
+gsap.to("#paralax", {
+    y: -2000,
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#paralax",
+        start: "top bottom", // the default values
+        end: "bottom top",
+        scrub: true
+    },
+});
+
+// parallax end
+
+// wangs start
 gsap.set("#bardial>*", { opacity: 0 })
-gsap.to("#bardial>*", { opacity: 1, duration: 0.05, repeat: -1, yoyo: true, stagger: { each: 0.02, from: "end" } })
-
-gsap.timeline({ repeat: -1, repeatDelay: 2 })
-    .to("#testicle1", { x: 20, duration: 0.1, repeat: 10, yoyo: true }, "testicle")
-    .to("#testicle1", { y: 20, duration: 0.05, repeat: 20, yoyo: true }, "testicle")
-    .to("#testicle2", { x: -20, duration: 0.1, repeat: 10, yoyo: true }, "testicle")
-    .to("#testicle2", { y: 20, duration: 0.05, repeat: 20, yoyo: true }, "testicle")
-
-const bubble = document.querySelector("#bubble")
-gsap.set(bubble, { transformOrigin: "center" })
-
-bubble.onmouseover = () => {
-    gsap.to(bubble, { scale: 2, duration: 4 })
-}
-bubble.onmouseleave = () => {
-    gsap.to(bubble, { scale: 1, duration: 4 })
-}
-
-
-const parallax = document.getElementById("paralax");
-
-window.addEventListener("scroll", function () {
-    let speedlimiter = 100;
-    if (window.screen.width <= 678) speedlimiter = 20;
-    let offset = this.window.pageYOffset;
-    gsap.to(parallax, { duration: 0.3, backgroundPositionY: offset / speedlimiter })
+gsap.set("#bubble", { transformOrigin: "center" })
+let timeline = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".wangs-section",
+        start: "start start",
+        // end: "2000px 500px",
+        scrub: true,
+        pin: true,
+        defaults: { ease: "none" }
+    },
 })
-console.log()
+
+timeline.to("#shine", { x: 800 }, "shine")
+    .to("#bardial>*", { opacity: 1, duration: 0.01, stagger: { each: 0.015, from: "end" } }, "shine")
+    .to("#bubble", { scale: 2, duration: .8 }, "shine");
+// wangs end
+
+// testicles start
+const testicles = document.getElementById("testicles");
+
+testicles.onclick = () => {
+    gsap.timeline()
+        .to("#testicle1, #testicle2", { scaleY: 0 })
+        .fromTo("#testicles_layer", { x: -10 }, { x: 10, duration: 0.1, repeat: 100, yoyo: true }, "testicle")
+        .to("#testicles_layer", { y: 20, duration: 0.05, repeat: 200, yoyo: true }, "testicle")
+        .to("#testicles_layer", { x: 0, y: 0 })
+}
+// testicles start
