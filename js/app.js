38dc72b5806d6214e229abe18e07eacb6b0b8807
minhaj57sorder <74466041+minhaj57sorder@users.noreply.github.com>
2022-04-24 22:44:27 +0600

    delivery 4rd

--- js/app.js
+++ js/app.js
@@ -28,44 +28,22 @@ collaps_menu.onclick = () => {
 }
 // menu expand collapse end
 
-// parallax start
-// window.addEventListener("scroll", function () {
-//     let speedlimiter = 150;
-//     if (window.screen.width <= 678) speedlimiter = 50;
-//     let offset = this.window.pageYOffset;
-//     gsap.to("#paralax", { backgroundPositionY: -1 * offset / speedlimiter })
-// })
-
-gsap.to("#paralax", {
-    y: -2000,
-    ease: "none",
-    scrollTrigger: {
-        trigger: "#paralax",
-        start: "top bottom", // the default values
-        end: "bottom top",
-        scrub: true
-    },
-});
-
-// parallax end
-
 // wangs start
-gsap.set("#bardial>*", { opacity: 0 })
-gsap.set("#bubble", { transformOrigin: "center" })
-let timeline = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".wangs-section",
-        start: "start start",
-        // end: "2000px 500px",
-        scrub: true,
-        pin: true,
-        defaults: { ease: "none" }
-    },
-})
+gsap.to("#shine", { x: 800, duration: 2, repeat: -1, repeatDelay: 3 })
 
-timeline.to("#shine", { x: 800 }, "shine")
-    .to("#bardial>*", { opacity: 1, duration: 0.01, stagger: { each: 0.015, from: "end" } }, "shine")
-    .to("#bubble", { scale: 2, duration: .8 }, "shine");
+gsap.set("#bardial>*", { opacity: 0 })
+gsap.to("#bardial>*", { opacity: 1, duration: 0.01, repeat: -1, repeatDelay: 0.5, yoyo: true, stagger: { each: 0.05, from: "end" } })
+
+const bubble = document.querySelector("#bubble")
+gsap.set(bubble, { transformOrigin: "center" })
+
+bubble.onmouseover = () => {
+    gsap.to(bubble, { scale: 2, duration: 4 })
+}
+bubble.onmouseleave = () => {
+    gsap.to(bubble, { scale: 1, duration: 4 })
+}
+
 // wangs end
 
 // testicles start
@@ -75,7 +53,7 @@ testicles.onclick = () => {
     gsap.timeline()
         .to("#testicle1, #testicle2", { scaleY: 0 })
         .fromTo("#testicles_layer", { x: -10 }, { x: 10, duration: 0.1, repeat: 100, yoyo: true }, "testicle")
-        .to("#testicles_layer", { y: 20, duration: 0.05, repeat: 200, yoyo: true }, "testicle")
+        .to("#testicles_layer", { y: -20, duration: 0.05, repeat: 200, yoyo: true }, "testicle")
         .to("#testicles_layer", { x: 0, y: 0 })
 }
 // testicles start
