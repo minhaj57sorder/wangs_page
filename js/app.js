ca1707ad6826c5759cfdcb4bd806fc20b079ac05
minhaj57sorder <74466041+minhaj57sorder@users.noreply.github.com>
2022-04-25 12:22:11 +0600

    delivery 6th

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 // faq expand collapse start
 const faq_heading = document.querySelectorAll(".faq-heading");
 faq_heading.forEach(e => {
+    var content = e.nextElementSibling;
+    if (e.parentNode.classList[1] !== "collapsed") {
+        content.style.marginTop = "24px";
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
     e.onclick = () => {
         e.parentNode.classList.toggle("collapsed")
-        var content = e.nextElementSibling;
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
             content.style.marginTop = "0px";
@@ -32,11 +36,11 @@ collaps_menu.onclick = () => {
 gsap.to("#shine", { x: 800, duration: 2, repeat: -1, repeatDelay: 3 })
 
 gsap.set("#bardial>*", { opacity: 0 })
-<<<<<<< HEAD
-gsap.to("#bardial>*", { opacity: 1, duration: 0.01, repeat: -1, repeatDelay: 0.5, stagger: { each: 0.05, from: "end" } })
-=======
-gsap.to("#bardial>*", { opacity: 1, duration: 0.01, repeat: 1, repeatDelay: 0.5, yoyo: true, stagger: { each: 0.05, from: "end" } })
->>>>>>> 345f1a81c0dcc01a5cfe839158b28b6822ccd5fc
+
+gsap.timeline({ repeat: -1, repeatDelay: 3, })
+    .to("#bardial>*", { opacity: 1, duration: 0.01, stagger: { each: 0.05, from: "end" } })
+    .to("#bardial>*", { opacity: 0, duration: 0.5 })
+
 
 const bubble = document.querySelector("#bubble")
 gsap.set(bubble, { transformOrigin: "center" })
